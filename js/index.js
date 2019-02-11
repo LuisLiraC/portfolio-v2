b5eb01613189a40993fe744fe47e02d6cf0ed7b2
LuisLiraC <luis_ssbm@hotmail.com>
2019-02-10 23:50:33 -0600

    Modal and JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,293 @@
 $('.navbar-nav a').on('click', function () {
     $('.navbar-collapse').removeClass('show');
 });
+
+const diplomas = document.getElementsByClassName("diploma");
+const modal_content = document.getElementById("modal-content")
+
+for (let i = 0; i < diplomas.length; i++){
+    diplomas[i].addEventListener('click', () => {
+        const data_diploma = event.target.getAttribute("data-diploma");
+        const dataStr = data_diploma.toString()
+        modal(dataStr);
+    });
+}
+
+function modal(data) {
+    if (data.indexOf('career') >= 0){
+        const index = data.split('-')[1];
+        const clicked = careers[index - 1];
+        var content = HTMLTemplate(clicked);
+
+    } else if (data.indexOf('frontend') >= 0) {
+        const index = data.split('-')[1];
+        const clicked = frontend[index - 1];
+        var content = HTMLTemplate(clicked);
+
+    } else if (data.indexOf('backend') >= 0) {
+        const index = data.split('-')[1];
+        const clicked = backend[index - 1];
+        var content = HTMLTemplate(clicked);
+
+    } else if (data.indexOf('generals') >= 0) {
+        const index = data.split('-')[1];
+        const clicked = generals[index - 1];
+        var content = HTMLTemplate(clicked);
+
+    } else if (data.indexOf('english') >= 0) {
+        const index = data.split('-')[1];
+        const clicked = english[index - 1];
+        var content = HTMLTemplate(clicked);
+
+    } else if (data.indexOf('design') >= 0) {
+        const index = data.split('-')[1];
+        const clicked = design[index - 1];
+        var content = HTMLTemplate(clicked);
+
+    } else if (data.indexOf('cms') >= 0) {
+        const index = data.split('-')[1];
+        const clicked = cms[index - 1];
+        var content = HTMLTemplate(clicked);
+    }
+
+    $('.MyModal').modal('show')
+    modal_content.innerHTML = content;
+}
+
+
+function HTMLTemplate(data){
+    return (
+        `
+        <h4>${data.title}</h4>
+        <img src="${data.src}" class="diploma-image">
+        <a class="btn btn-success view" href="${data.link}" target="_blank">Ver en Platzi</a>
+        `
+    )
+}
+
+const careers = [ 
+    {   
+        id: 1,
+        title: "Carrera de Arquitectura Frontend",
+        src: "img/careers/png/diplomas/diploma-arquitecto.png",
+        link: "https://platzi.com/@Luis_LiraC/carrera/42-arquitecto/diploma/detalle/"
+    },
+    {
+        id: 2,
+        title: "Carrera de Desarrollo con WordPress",
+        src: "img/careers/png/diplomas/diploma-desarrollo-wordpress.png",
+        link: "https://platzi.com/@Luis_LiraC/carrera/52-desarrollo-wordpress/diploma/detalle/"
+    }
+    
+]
+
+const frontend = [ 
+    {   
+        id: 1,
+        title: "Curso de Desarrollo Web Online",
+        src: "img/courses/png/diplomas/frontend/diploma-html5-css3.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1350-html5-css3/diploma/detalle/"
+    },
+    {
+        id: 2,
+        title: "Curso de CSS Grid Layout",
+        src: "img/courses/png/diplomas/frontend/diploma-css-grid-layout.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1229-css-grid-layout/diploma/detalle/"
+    },
+    {
+        id: 3,
+        title: "Curso de Responsive Design",
+        src: "img/courses/png/diplomas/frontend/diploma-responsive-design.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1362-responsive-design/diploma/detalle/"
+    },
+    {
+        id: 4,
+        title: "Curso de Animanciones para la Web",
+        src: "img/courses/png/diplomas/frontend/diploma-animaciones-web.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1103-animaciones-web/diploma/detalle/"
+    },
+    {
+        id: 5,
+        title: "Curso de SASS",
+        src: "img/courses/png/diplomas/frontend/diploma-sass.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1244-sass/diploma/detalle/"
+    },
+    {
+        id: 6,
+        title: "Curso de Stylus",
+        src: "img/courses/png/diplomas/frontend/diploma-stylus.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1245-stylus/diploma/detalle/"
+    },
+    {
+        id: 7,
+        title: "Curso de LESS",
+        src: "img/courses/png/diplomas/frontend/diploma-less.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1246-less/diploma/detalle/"
+    },
+    {
+        id: 8,
+        title: "Curso de Bootstrap",
+        src: "img/courses/png/diplomas/frontend/diploma-bootstrap.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1331-bootstrap/diploma/detalle/"
+    },
+    {
+        id: 9,
+        title: "Curso de Foundation",
+        src: "img/courses/png/diplomas/frontend/diploma-foundation.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1332-foundation/diploma/detalle/"
+    },
+    {
+        id: 10,
+        title: "Curso de PostCSS",
+        src: "img/courses/png/diplomas/frontend/diploma-postcss.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1242-postcss/diploma/detalle/"
+    },
+    {
+        id: 11,
+        title: "Curso de Bots con Facebook Messenger",
+        src: "img/courses/png/diplomas/frontend/diploma-bots-fb.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1355-bots-fb/diploma/detalle/"
+    },
+    {
+        id: 12,
+        title: "Curso de Optimización de Páginas Web",
+        src: "img/courses/png/diplomas/frontend/diploma-optimizacion-web.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1390-optimizacion-web/diploma/detalle/"
+    },
+    {
+        id: 13,
+        title: "Curso de Sistemas de Diseño para Desarrolladores",
+        src: "img/courses/png/diplomas/frontend/diploma-diseno-desarrolladores.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1422-diseno-desarrolladores/diploma/detalle/"
+    },
+    {
+        id: 14,
+        title: "Curso Profesional de Desarrollo Web",
+        src: "img/courses/png/diplomas/frontend/diploma-web-avanzado.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1492-web-avanzado/diploma/detalle/"
+    }
+    
+]
+
+const backend = [ 
+    {   
+        id: 1,
+        title: "Curso de jQuery a JavaScript",
+        src: "img/courses/png/diplomas/backend/diploma-jquery-js.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1316-jquery-js/diploma/detalle/"
+    },
+    {
+        id: 2,
+        title: "Curso de Fundamentos de JavaScript",
+        src: "img/courses/png/diplomas/backend/diploma-fundamentos-javascript.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1339-fundamentos-javascript/diploma/detalle/"
+    },
+    {
+        id: 3,
+        title: "Curso de Introducción a PHP",
+        src: "img/courses/png/diplomas/backend/diploma-php.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1338-php/diploma/detalle/"
+    }
+]
+
+const generals = [ 
+    {   
+        id: 1,
+        title: "Curso Profesional de Git y GitHub",
+        src: "img/courses/png/diplomas/generals/diploma-git-github.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1170-git-github/diploma/detalle/"
+    },
+    {
+        id: 2,
+        title: "Fundamentos de Ingeniería de Software",
+        src: "img/courses/png/diplomas/generals/diploma-ingenieria.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1098-ingenieria/diploma/detalle/"
+    },
+    {
+        id: 3,
+        title: "Introducción a Terminal y Línea de Comandos",
+        src: "img/courses/png/diplomas/generals/diploma-terminal.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1276-terminal/diploma/detalle/"
+    },
+    {
+        id: 4,
+        title: "Fundamentos de Bases de Datos",
+        src: "img/courses/png/diplomas/generals/diploma-bd.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1183-bd/diploma/detalle/"
+    },
+    {
+        id: 5,
+        title: "Curso de Programación Básica",
+        src: "img/courses/png/diplomas/generals/diploma-programacion-basica.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1050-programacion-basica/diploma/detalle/"
+    },
+    {
+        id: 6,
+        title: "Curso de Redes de Internet",
+        src: "img/courses/png/diplomas/generals/diploma-redes.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1277-redes/diploma/detalle/"
+    },
+    {
+        id: 7,
+        title: "Curso de Marca Personal",
+        src: "img/courses/png/diplomas/generals/diploma-marca-personal.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1220-marca-personal/diploma/detalle/"
+    },
+    {
+        id: 8,
+        title: "Curso de Finanzas Personales",
+        src: "img/courses/png/diplomas/generals/diploma-finanzas.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1190-finanzas/diploma/detalle/"
+    },
+]
+
+const english = [ 
+    {   
+        id: 1,
+        title: "Curso de Inglés Básico",
+        src: "img/courses/png/diplomas/english/diploma-ingles-basico.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1358-ingles-basico/diploma/detalle/"
+    },
+    {
+        id: 2,
+        title: "Curso de Inglés Básico: Gramática",
+        src: "img/courses/png/diplomas/english/diploma-ingles-gramatica.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1370-ingles-gramatica/diploma/detalle/"
+    },
+    {
+        id: 3,
+        title: "Curso de Inglés Básico: Conversación",
+        src: "img/courses/png/diplomas/english/diploma-ingles-conversacion.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1371-ingles-conversacion/diploma/detalle/"
+    },
+    {
+        id: 4,
+        title: "Curso de Escritura de Inglés",
+        src: "img/courses/png/diplomas/english/diploma-escritura-ingles.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1279-escritura-ingles/diploma/detalle/"
+    }
+]
+
+const design = [ 
+    {   
+        id: 1,
+        title: "Curso de Sistemas de Diseño",
+        src: "img/courses/png/diplomas/design/diploma-sistemas-diseno.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1420-sistemas-diseno/diploma/detalle/"
+    }
+]
+
+const cms = [ 
+    {   
+        id: 1,
+        title: "Curso de Temas y Plugins en WordPress",
+        src: "img/courses/png/diplomas/cms/diploma-wordpress-temas.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1154-wordpress/diploma/detalle/"
+    },
+    {   
+        id: 2,
+        title: "Curso de Creación y Optimización de Sitios Web con WordPress",
+        src: "img/courses/png/diplomas/cms/diploma-seo-wordpress.png",
+        link: "https://platzi.com/@Luis_LiraC/curso/1433-seo-wordpress/diploma/detalle/"
+    }
+]
